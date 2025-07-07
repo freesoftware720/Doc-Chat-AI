0af4ede813a59f61fbc4bedb0040dca410ddfba8
Free Software <freesoftware720@gmail.com>
2025-07-07 21:47:53 +0000

    ⚠ `experimental.serverComponentsExternalPackages` has been moved to `ser

---
+++ src/next.config.ts
@@ -0,0 +1,25 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  serverExternalPackages: ['pdf-parse'],
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
